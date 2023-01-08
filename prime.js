61f37da5db0393b7870df406bcd27aff9dfdedb4
unknown <negiammu15@gmail.com>
2023-01-08 11:53:52 -0800

    prime number code

---
+++ prime.js
@@ -0,0 +1,16 @@
+
+let number=5;
+
+let factor=0;
+
+for(let i=0; i<=5; i++){
+	
+	if(5%i==0){
+	   factor++;
+	}
+}
+	if(factor==2){
+	console.log("prime number");	
+}else{
+	console.log("not a prime number");
+}
